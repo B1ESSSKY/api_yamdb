448019c2a5be1454a0a762a3e2336ca126758000
QChan <>
2025-03-06 22:17:50 +0300

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# api_yamdb
-api_yamdb
+# API YaMDb
+## Запуск проекта
+1. Клонируем репозиторий  
+```  
+git clone git@github.com:TheQChan/api_final_yatube.git  
+```
+2. Переходим в папку, создаем и активируем виртуальное окружение
+```  
+python -m venv venv
+```
+```  
+source venv/Scripts/activate
+```
+3. Обновляем pip и устанавливаем зависимости
+```  
+python -m pip install --upgrade pip
+```
+```  
+pip install -r requirements.txt
+```
+4. Выполнение миграций
+```  
+python manage.py migrate
+```
+5. Запуск проекта
+```  
+python manage.py runserver
+```
--- api_yamdb/api/v1/permissions.py
+++ api_yamdb/api/v1/permissions.py
@@ -13,14 +13,12 @@ class IsAdminModeratorAuthorOrReadOnly(permissions.BasePermission):
     """Доступ только администраторам, модераторам и авторам."""
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or (
-                request.user.is_authenticated and (
+        return request.method in permissions.SAFE_METHODS or (
+            request.user.is_authenticated
+            and (
                 request.user.is_admin
                 or request.user.is_moderator
                 or request.user == obj.author
-            )
             )
         )
 
