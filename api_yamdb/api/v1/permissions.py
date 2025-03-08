a73dbb75b919530d627fcd0f2e600c968114d8f1
Egor Dubkov <b1essssky@gmail.com>
2025-03-08 13:35:31 +0300

    Feature/reviews-and-comments. Исправил пермишшены.

--- api_yamdb/api/v1/permissions.py
+++ api_yamdb/api/v1/permissions.py
@@ -6,8 +6,9 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     """Доступ только администраторам. Для остальных только для чтения."""
 
     def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_admin)
+        return request.method in SAFE_METHODS or (
+            request.user.is_authenticated and request.user.is_admin
+        )
 
 
 class IsAdminModeratorAuthorOrReadOnly(permissions.BasePermission):
@@ -17,7 +18,7 @@ class IsAdminModeratorAuthorOrReadOnly(permissions.BasePermission):
         return request.method in SAFE_METHODS or request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return request.method in permissions.SAFE_METHODS or (
+        return request.method in SAFE_METHODS or (
             request.user.is_authenticated
             and (
                 request.user.is_admin
