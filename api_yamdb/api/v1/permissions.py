e6d1d7ef618f28914d61174ad734371de465060a
Egor Dubkov <b1essssky@gmail.com>
2025-03-09 17:54:52 +0300

    Исправления flake8

--- api_yamdb/api/v1/permissions.py
+++ api_yamdb/api/v1/permissions.py
@@ -33,8 +33,8 @@ class IsAdminOrSuperUser(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-            request.user.is_authenticated and
-            (
+            request.user.is_authenticated
+            and (
                 request.user.is_admin or
                 request.user.is_superuser
             )
