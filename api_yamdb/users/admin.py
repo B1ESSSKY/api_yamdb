1471154c2c5cbb2188d71a29383793e5844519c3
Egor Dubkov <b1essssky@gmail.com>
2025-03-11 00:14:51 +0300

    Улучшил админ-зону для работы с пользователями.

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -7,8 +7,27 @@ from users.models import User
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     list_display = (
-        'pk', 'username', 'email', 'first_name', 'last_name', 'role', 'bio'
+        'username', 'email', 'first_name', 'last_name', 'role', 'bio'
     )
     search_fields = ('username', 'email', 'first_name', 'last_name', 'role')
     list_filter = ('role',)
+    list_editable = ('role',)
     empty_value_display = '-пусто-'
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Личные данные', {
+            'fields': (
+                'first_name', 'last_name', 'email', 'bio')
+        }),
+        ('Роль', {'fields': ('role',)}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('username', 'password1', 'password2')}),
+        ('Личные данные', {
+            'fields': (
+                'first_name', 'last_name', 'email', 'bio')
+        }),
+        ('Роль', {'fields': ('role',)}),
+    )
